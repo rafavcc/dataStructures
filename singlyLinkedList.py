e7e1204b405acc81a51a9b886059f3fcff8fc64b
Rafael Viegas <rafavcc@gmail.com>
2025-01-27 16:19:14 -0300

    Implementing functions to insert nodes

--- singlyLinkedList.py
+++ singlyLinkedList.py
@@ -26,6 +26,16 @@ class LinkedList:
                     break
                 lastNode = lastNode.next
             lastNode.next = newNode
+            
+    def insertAsHead(self, newNode):
+        # Store current head temporarily in another structure
+        temporaryNode = self.head
+        # New Node is the new head
+        self.head = newNode
+        # New head points to temporary node, the former head
+        self.head.next = temporaryNode 
+        # Delete temporary node
+        del temporaryNode
 
 # Print linked list
     def printLinkedList(self):
@@ -42,13 +52,32 @@ class LinkedList:
             iterador = iterador.next
         print(iterador.data)
 
+    def insertAt(self, newNode, position):
+        # if position == 0:
+        #     self.insertAsHeas(newNode)
+        # else:
+        currentNode = self.head
+        currentPosition = 0
+        while True:
+            if currentPosition == position:
+                previousNode.next = newNode
+                newNode.next = currentNode
+                break
+            previousNode = currentNode
+            currentNode = currentNode.next
+            currentPosition += 1
+            
 
 # First node with name but point to none
-firstNode = Node("Rafael")
-secondNode = Node("Fernanda")
-thirdNode = Node("Ligia")
+firstNode = Node("One")
+secondNode = Node("Two")
+thirdNode = Node("Three")
 linkedList = LinkedList()
 linkedList.insert(firstNode)
 linkedList.insert(secondNode)
 linkedList.insert(thirdNode)
+fourthNode = Node("Zero")
+linkedList.insertAsHead(fourthNode)
+fifthNode = Node("Zero_point_five")
+linkedList.insertAt(fifthNode, 3)
 linkedList.printLinkedList()
